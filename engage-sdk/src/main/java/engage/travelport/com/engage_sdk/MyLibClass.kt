f8d7df2eb5c644f8c9cd44ae5c634fe6fcbc6bc0
Maria Colomina <Maria.Colomina@endava.com>
2018-10-23 14:53:32 +0300

    init commit

---
+++ engage-sdk/src/main/java/engage/travelport/com/engage_sdk/MyLibClass.kt
@@ -0,0 +1,12 @@
+package engage.travelport.com.engage_sdk
+
+import android.util.Log
+
+class MyLibClass {
+
+  companion object {
+    fun provideLog() {
+      Log.e("error","errorMessage")
+    }
+  }
+}
